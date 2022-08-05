32cbf55ca1489bbf589599120c036970eb04d9c3
Norbert Karp <norbertkarp2@gmail.com>
2022-08-05 15:03:32 +0200

    each article

--- js/script.js
+++ js/script.js
@@ -38,4 +38,40 @@ const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
+
+
+  const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+
+  /* for each article */
+
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  for(let article of articles){
+    console.log(article)
+  }
+
+    /* get the article id */
+
+    const articleID = article.getAttribute('id')
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+
+}
+
+generateTitleLinks();
 }
